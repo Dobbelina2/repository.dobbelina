e8047b527e53cbfdf4d18255903bd9eba53051c7
Dobbelina2 <132213227+Dobbelina2@users.noreply.github.com>
2023-05-27 00:44:00 +0000

    Update 6xtube.py

--- plugin.video.cumination/resources/lib/sites/6xtube.py
+++ plugin.video.cumination/resources/lib/sites/6xtube.py
@@ -2,7 +2,7 @@
     Cumination
     Copyright (C) 2022 Team Cumination
 
-    This program is free software: you can redistribute it and/or modify
+    This program is free softwares: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
